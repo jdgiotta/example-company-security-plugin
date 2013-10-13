5c2bf087bef90d007b8c2d16be47f4d3b6814c89
John D Giotta <jgiotta@hdlabinc.com>
2013-10-12 22:02:48 -0400

    Wrapped up missing config settings. - Added RegisterStdUser ApplicationListener - Implemented additional LoginController action

--- src/groovy/com/mycompany/RegisterStdUser.groovy
+++ src/groovy/com/mycompany/RegisterStdUser.groovy
@@ -1,11 +1,19 @@
 package com.mycompany
 
-/**
- * Created with IntelliJ IDEA.
- * User: John
- * Date: 10/12/13
- * Time: 9:16 PM
- * To change this template use File | Settings | File Templates.
- */
-class RegisterStdUser {
+import org.springframework.context.ApplicationListener
+import org.springframework.security.authentication.event.InteractiveAuthenticationSuccessEvent
+import org.apache.juli.logging.LogFactory
+
+class RegisterStdUser implements ApplicationListener<InteractiveAuthenticationSuccessEvent> {
+    private static final log = LogFactory.getLog(this)
+    def sessionFactory
+    void onApplicationEvent(InteractiveAuthenticationSuccessEvent event) {
+        log.debug "Successfully logged in..."
+        StdUser.withTransaction {
+            StdUser.findByUsername(event.source.principal?.getAt('username')) ?:
+                new StdUser(username: event.source.principal?.getAt('username'),
+                        password: 'KU)(&#U)(&(#UIHAHQD&*Y*(Y@(EHY*(QY(*@YH#*H',
+                        enabled: true).save(failOnError: true)
+        }
+    }
 }
